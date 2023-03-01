f7e2f1c775d8ddc93c74a90861f86ed7afb91c87
NicPWNs <nic@nicpjones.com>
2023-03-01 10:53:22 -0500

    doc: update README

--- random_unicode_emoji/__init__.py
+++ random_unicode_emoji/__init__.py
@@ -1,16 +1,13 @@
-# Change the content according to your package.
-# from directory.filename.py import classname
 from random_unicode_emoji.random_unicode_emoji import random_emoji
 
 __author__ = 'Nic Jones'
 __email__ = 'nic@nicpjones.com'
-__version__ = '1.3'
-
-# module level doc-string
+__version__ = '1.0.0'
 __doc__ = """
 Random Unicode Emoji
 ====================
-A simple Python package to retrieve a random Unicode emoji.
+A Python package and function to retrieve a random Unicode emoji.
+
 Install
 -------
 >>> pip install random-unicode-emoji
