0598ddde1006dce3f89734501a20c572483478fe
NicPWNs <nic@nicpjones.com>
2023-02-28 20:07:26 -0500

    doc: update README

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-from setuptools import setup, find_packages
+import setuptools
+import re
 
+VERSIONFILE="src/random_emoji/__init__.py"
+getversion = re.search( r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
+if getversion:
+    new_version = getversion.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-setup(
-    name='random_unicode_emoji',
-    version='1.1',
-    license='MIT',
-    author="Nic Jones",
-    author_email='nic@nicpjones.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    url='https://github.com/NicPWNs/random-unicode-emoji',
-    keywords='random unicode emoji',
-)
+with open("README.rst", "r") as r:
+    long_description = r.read()
+    setuptools.setup(
+        name='random_unicode_emoji',
+        python_requires='>=3',
+        version=new_version,
+        license='MIT',
+        author="Nic Jones",
+        author_email='nic@nicpjones.com',
+        description='A simple Python package to retrieve a random Unicode emoji. 😄',
+        long_description=long_description,
+        packages=setuptools.find_packages(),
+        package_dir={'': 'src'},
+        url='https://github.com/NicPWNs/random-unicode-emoji',
+        download_url ='https://github.com/NicPWNs/random-unicode-emoji/releases',
+        keywords='random unicode emoji',
+    )
---
+++ src/random_emoji/__init__.py
@@ -0,0 +1,28 @@
+# Change the content according to your package.
+# from directory.filename.py import classname
+from random_emoji.random_emoji import random_emoji
+
+__author__ = 'Nic Jones'
+__email__ = 'nic@nicpjones.com'
+__version__ = '1.2'
+
+# module level doc-string
+__doc__ = """
+Random Unicode Emoji
+====================
+
+Description
+-----------
+
+A simple Python package to retrieve a random Unicode emoji. 😄
+
+
+Usage
+-----
+
+>>> # Import the Library
+>>> import random-unicode-emoji
+>>> # Use the Function
+>>> emoji = random_emoji()
+
+"""
